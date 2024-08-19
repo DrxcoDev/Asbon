b27c8ff4b0320143af28b5ec963489bf20b11983
DrxcoDev <156522801>
2024-08-19 19:24:36 +0200

    Commit

---
+++ src/lib/cout.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fs>
+
+
+
+#define OS
+#define SYSTEM
+#define __OVERLOAD__
+#define LIBRARY
+
+
+int main() {
+    char main = fs::READ();
+
+    for ( i = 0; i < 10; i++)
+    {
+        if (fs != __DEFINE_CPP_OVERLOAD_STANDARD_FUNC_0_0_EX){
+            if(_get_printf_count_output == int){
+                printf("%s ^[(0x111)]");
+            }
+        }   else {
+            printf(_FUNCTEXCEPT_H);
+
+        }
+        
+    }
+    
+}
